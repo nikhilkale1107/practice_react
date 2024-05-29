a15e2c7daf6a87e0e6695352f07c8181a24ed73c
Nikhil Kale <nikhilkale1107@gmail.com>
2024-05-29 23:01:15 +0530

    List of users are displayed on screen.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  function addUserHandler (uName, uAge) {
+    setUsersList((prevUserList) => {
+      return [...prevUserList, { name: uName, age: uAge, id: Math.random().toString() }];
+    });
+
+  }
+
   return (
     <div className='App'>
-      <AddUser/>
+      <AddUser onAddUser={addUserHandler}/>
+      <UserList users={usersList}/>
     </div>
   );
 }
